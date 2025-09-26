850f752622ecd4f4a9368dc354d5497a0d902d70
Даниил Ковалевич <dan@aa-a.ru>
2025-09-26 10:47:34 +0300

    feat: routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css';
+import { BrowserRouter, Route } from 'react-router';
+import { PageAuth } from './Pages/PageAuth';
+import { PageCalendar } from './Pages/PageCalendar';
 
 function App() {
   return (
-    <div className="App">
-      <div>app</div>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Route path="/" element={<PageAuth/>}/>
+        <Route path="/calendar" element={<PageCalendar/>} />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/Pages/PageAuth.tsx
+++ frontend/src/Pages/PageAuth.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 export const PageAuth = () => {
+    let navigate = useNavigate()
     return <></>
 }
