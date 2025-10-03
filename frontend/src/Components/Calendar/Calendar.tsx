95c09dd2b6a7dadf19dc350bfd26ff04241d8c35
Даниил Ковалевич <dan@aa-a.ru>
2025-10-03 22:58:02 +0300

    feat: basic calendar UI (updated)

--- frontend/src/Components/Calendar/Calendar.tsx
+++ frontend/src/Components/Calendar/Calendar.tsx
@@ -22,25 +22,54 @@ enum MonthNames {
   DEC = "декабрь"
 }
 
+enum WeekDays {
+  MON = "понедельник",
+  TUE = "вторник",
+  WED = "среда",
+  THU = "четверг",
+  FRI = "пятница",
+  SAT = "суббота",
+  SUN = "воскресенье"
+}
+
 export const Calendar = () => {
-    
-    let date = new Date();
-    let [currMonth, setMonth] = useState(date.getMonth() + 1);
-    let monthStart = new Date(date.getFullYear(), date.getMonth(), 1)
-    let monthEnd = new Date(date.getFullYear(), date.getMonth() + 1, 0)
+    const monthNames = Object.values(MonthNames)
+    const weekDays = Object.values(WeekDays)
+
+    let [currentDate, setDate] = useState(new Date())
+    let monthStart = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1)
+    let monthEnd = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0)
     let currMonthDays = [];
+
+    let startDay = monthStart.getDay() === 0 ? 6 : monthStart.getDay() - 1;
 
     for (let i = monthStart.getDate(); i <= monthEnd.getDate(); i++) {
       let day = {
-        date : new Date(date.getFullYear(), date.getMonth(), i)
+        date : new Date(currentDate.getFullYear(), currentDate.getMonth(), i)
       }
       currMonthDays.push(day)
     }
 
+    for (let i = 0; i < startDay; i++) {
+      currMonthDays.unshift({date : null})
+    }
+
+    let endDay = monthEnd.getDay();
+    let adjustedEndDay = endDay === 0 ? 0 : 7 - endDay;
+
+    for (let i = 0; i < adjustedEndDay; i++) {
+      currMonthDays.push({date : null})
+    }
+
     return (
         <>
-          <div>{currMonth}</div>
-          {currMonthDays.map(el => <div>{el.date.getDate()}</div>)}
+          <div>{monthNames[currentDate.getMonth()]} {currentDate.getFullYear()}</div>
+          <button onClick={() => setDate(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1))}>назад</button>
+          <button onClick={() => setDate(new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1))}>вперед</button>
+          <section className="container">
+            {weekDays.map(el => <div>{el}</div>)}
+            {currMonthDays.map(el => <div className="day-card">{el.date != null ? el.date?.getDate() : "-"}</div>)}
+          </section>
         </>
     );
 };
