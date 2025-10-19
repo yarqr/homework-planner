3a59dae5e9736104f78d41ea7d03e96faa5cc921
YarQR <rosya6721@gmail.com>
2025-10-19 21:35:11 +0300

    fix: delete task didn't work

--- backend/api/routes/tasks/delete.py
+++ backend/api/routes/tasks/delete.py
@@ -2,20 +2,15 @@ from typing import Annotated
 from uuid import UUID
 
 from fastapi import HTTPException, Path, Request, status
-from pydantic import BaseModel
 
 from backend.db.repositories import TaskRepository
 
 
-class DeleteByIdRequest(BaseModel):
-    task_id: Annotated[UUID, Path(title="Идентификатор задачи")]
-
-
 async def delete_by_id(
-    data: DeleteByIdRequest,
+    task_id: Annotated[UUID, Path(title="Идентификатор задачи")],
     request: Request,
 ) -> None:
     task_repo: TaskRepository = request.app.state.task_repo
-    state = task_repo.delete(data.task_id)
+    state = task_repo.delete(task_id)
     if not state:
         raise HTTPException(status.HTTP_404_NOT_FOUND, "Task not found")
